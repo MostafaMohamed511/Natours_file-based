0ca301eda0b097e7bd119fd66292ecc14e4d066b
MostafaMohamed511 <mostafa.fcai.gmail.com>
2023-07-31 22:06:35 +0300

    finished CRUD operations on tours

--- app.js
+++ app.js
@@ -10,37 +10,74 @@ const toursJson = fs.readFileSync(
 const tours = JSON.parse(toursJson);
 
 app.get('/', (request, response) => {
-  console.log('fuck oyoy');
   response.status(200).send('hello!!!');
 });
 
 app.get('/tours', (req, res) => {
-  res.status(201).json({
-    status: 'fuck you',
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
     data: {
       tours: { tours },
     },
   });
 });
+
+app.get('/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  if (id >= tours.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+
+  const tour = tours[id];
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
 app.post('/tours', (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   const newObj = Object.assign({ id: tours.length }, req.body);
   tours.push(newObj);
-  (async () => {
-    await fs.writeFile(
-      `${__dirname}/dev-data/data/tours-simple.json`,
-      JSON.stringify(tours),
-      (err) => {
-        res.status(201).json({
-          states: 'success',
-          data: { newObj },
-        });
-        console.log('writing in');
-      }
-    );
-    console.log('writing');
-  })();
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        states: 'success',
+        data: newObj,
+      });
+    }
+  );
 });
+
+app.patch('/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  res.status(200).json({
+    status: 'updated',
+    data: {
+      tour: '<Updated tour>',
+    },
+  });
+});
+
+app.delete('/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  res.status(204).send();
+});
+
 app.listen(portNum, () => {
   console.log('listing...');
 });
